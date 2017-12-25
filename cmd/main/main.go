9a452d7fe0513080d1ddcaf2c35242dd062932ea
Milan Aleksic <milanaleksic@gmail.com>
2017-12-25 12:57:38 +0100

    handling updates as well

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -32,54 +32,39 @@ func main() {
 
 	client := dropbox.NewClient(*token, *path)
 	go client.Process()
-	mainLoop(client, signalChannel, wiki)
+	app := &mainApp{
+		client:        client,
+		signalChannel: signalChannel,
+		wiki:          wiki,
+	}
+	app.mainLoop()
 }
 
-func mainLoop(client *dropbox.Client, c chan os.Signal, wiki *dokuwiki.Client) {
-	contentsData, err := wiki.GetPage("dropbox_sync")
+type mainApp struct {
+	contentsData  string
+	client        *dropbox.Client
+	signalChannel chan os.Signal
+	wiki          *dokuwiki.Client
+}
+
+func (m *mainApp) mainLoop() {
+	contentsDataLocal, err := m.wiki.GetPage("dropbox_sync")
 	if err != nil {
 		log.Fatalf("Failed to fetch page contents from wiki: %+v", err)
 	}
+	m.contentsData = contentsDataLocal
 	for {
 		select {
-		case f := <-client.ChangedFile:
+		case f := <-m.client.ChangedFile:
 			if f.Initial {
 				log.Printf("initial file: %+v, data length=%v", f.Name, len(f.Data))
-				if strings.TrimSpace(string(f.Data)) == "" {
-					continue
-				}
-				pagename := niceName(f.Name)
-				if !strings.Contains(contentsData, fmt.Sprintf("(%s)", pagename)) {
-					log.Printf("Adding link to %s (%s) to the contents page", pagename, f.Name)
-					contentsData = contentsData + fmt.Sprintf("\n- [%s](%s)", f.Name, pagename)
-					success, err := wiki.PutPage("dropbox_sync", contentsData)
-					if err != nil {
-						log.Fatalf("Failed to store page in wiki: %+v", err)
-					}
-					if !success {
-						log.Println("Failed to store page in wiki")
-					}
-				}
-				currentPage, err := wiki.GetPage(pagename)
-				if err != nil {
-					log.Fatalf("Failed to get page %s from wiki: %+v", pagename, err)
-				}
-				if string(f.Data) == currentPage {
-					log.Printf("Same contents detected for %v, skipping", f.Name)
-				} else {
-					success, err := wiki.PutPage(pagename, string(f.Data))
-					if err != nil {
-						log.Fatalf("Failed to store page %s in wiki: %+v", pagename, err)
-					}
-					if !success {
-						log.Println("Failed to store page in wiki")
-					}
-				}
+				m.updateFileInWiki(f)
 			} else {
-				log.Printf("changed file: %+v", f.Name)
+				log.Printf("changed file: %+v, data length=%v", f.Name, len(f.Data))
+				m.updateFileInWiki(f)
 			}
 			break
-		case <-c:
+		case <-m.signalChannel:
 			os.Exit(0)
 			break
 		}
@@ -90,4 +75,37 @@ var titleReplacer = regexp.MustCompile("[^a-zA-Z0-9]")
 
 func niceName(s string) string {
 	return titleReplacer.ReplaceAllString(strings.ToLower(s), "_")
+}
+
+func (m *mainApp) updateFileInWiki(f dropbox.ChangedFile) {
+	if strings.TrimSpace(string(f.Data)) == "" {
+		return
+	}
+	pagename := niceName(f.Name)
+	if !strings.Contains(m.contentsData, fmt.Sprintf("(%s)", pagename)) {
+		log.Printf("Adding link to %s (%s) to the contents page", pagename, f.Name)
+		m.contentsData = m.contentsData + fmt.Sprintf("\n- [%s](%s)", f.Name, pagename)
+		success, err := m.wiki.PutPage("dropbox_sync", m.contentsData)
+		if err != nil {
+			log.Fatalf("Failed to store page in wiki: %+v", err)
+		}
+		if !success {
+			log.Println("Failed to store page in wiki")
+		}
+	}
+	currentPage, err := m.wiki.GetPage(pagename)
+	if err != nil {
+		log.Fatalf("Failed to get page %s from wiki: %+v", pagename, err)
+	}
+	if string(f.Data) == currentPage {
+		log.Printf("Same contents detected for %v, skipping", f.Name)
+	} else {
+		success, err := m.wiki.PutPage(pagename, string(f.Data))
+		if err != nil {
+			log.Fatalf("Failed to store page %s in wiki: %+v", pagename, err)
+		}
+		if !success {
+			log.Println("Failed to store page in wiki")
+		}
+	}
 }
